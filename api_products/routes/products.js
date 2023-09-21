edd21695471da225275352c6ccd90328d0aff361
Sam Vitare <sandhya.palanki@gmail.com>
2023-09-21 09:59:53 -0500

    added api_products

---
+++ api_products/routes/products.js
@@ -0,0 +1,11 @@
+let express = require("express");
+let products = require("./data.js");
+
+let router = express.Router();
+
+router.get("/products", function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  return res.json(products);
+});
+
+module.exports = router;
